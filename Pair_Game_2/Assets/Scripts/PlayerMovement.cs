22ba26b581e52879786fcd51854e9ee16acb7e9e
ka2ki2 <59772200+ka2-ki2@users.noreply.github.com>
2024-10-16 19:37:45 -0400

    extremely extremely basic platformer mechanics
    the jump is currently Flappy Bird

---
+++ Pair_Game_2/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] float jumpHeight;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        //move sideways
+        float horiz = Input.GetAxis("Horizontal") * speed;
+        rb.velocity = new Vector3(horiz, rb.velocity.y, 0);
+
+        //boing boing! this currently jumps infinitely haha
+        if(Input.GetKeyDown(KeyCode.UpArrow)) rb.AddForce(new Vector3(0, jumpHeight, 0));
+    }
+}
