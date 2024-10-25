55b5abc55c9924f4450058dcb3dd9c25c2c2abef
ka2ki2 <59772200+ka2-ki2@users.noreply.github.com>
2024-10-25 02:17:29 -0400

    trying to animate player

--- Pair_Game_2/Assets/Scripts/Intro/PlayerMovementIntro.cs
+++ Pair_Game_2/Assets/Scripts/Intro/PlayerMovementIntro.cs
@@ -6,6 +6,7 @@ public class PlayerMovementIntro : MonoBehaviour
 {
     [SerializeField] float speed;
     [SerializeField] float jumpHeight;
+    Animator anim;
     //public LayerMask platforms;
     Rigidbody2D rb;
     Vector3 spawnpoint;
@@ -13,6 +14,7 @@ public class PlayerMovementIntro : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
         spawnpoint = new Vector3(0, 0, 0);
     }
 
@@ -30,12 +32,33 @@ public class PlayerMovementIntro : MonoBehaviour
         
         //jump if on the ground
         if(Input.GetKeyDown(KeyCode.UpArrow) && Grounded()) rb.AddForce(new Vector3(0, jumpHeight, 0));
+        Animate(horiz / speed);
+    }
+
+    void Animate(float horiz)
+    {
+        if(!Grounded())
+        {
+            anim.Play("Jump");
+            Debug.Log("Not grounded");
+        }
+        else if(horiz > 0.1f || horiz < -0.1)
+        {
+             anim.Play("Player Walk");
+             if(horiz < 0) transform.localScale = new Vector3(-1f, 1f, 1f);
+             else transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+        else
+        {
+            anim.Play("Player Idle");
+        }
+
     }
     
     //checks if currently on the ground
     bool Grounded()
     {
-        return Physics2D.Raycast(transform.position, Vector2.down, 1.0f, 1<<6); //ground layer is currently hard-coded to 6
+        return Physics2D.Raycast(transform.position, Vector2.down, 5.0f, 1<<6); //ground layer is currently hard-coded to 6
     }
 
     public void setSpawn(float x, float y, float z)
